2aac4ad281452448d1ff8f75350937144b18a01b
wcohen64 <wcohen64@gmail.com>
2015-12-10 23:55:56 +0100

    work on suggestion activity

--- app/src/main/java/com/caseywaldren/downfordinner/InitialResponseActivity.java
+++ app/src/main/java/com/caseywaldren/downfordinner/InitialResponseActivity.java
@@ -2,12 +2,41 @@ package com.caseywaldren.downfordinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class InitialResponseActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnYes)
+    Button btnYes;
+
+    @Bind(R.id.btnNo)
+    Button btnNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_response);
+    }
+
+    @OnClick(R.id.btnYes)
+    public void respondYes() {
+        ParseUser.getCurrentUser().put("downForDinner", true);
+        notifyGroup();
+    }
+
+    @OnClick(R.id.btnNo)
+    public void respondNo() {
+        ParseUser.getCurrentUser().put("downForDinner", false);
+        notifyGroup();
+    }
+
+    public void notifyGroup(){
+
     }
 }
--- app/src/main/java/com/caseywaldren/downfordinner/SuggestionActivity.java
+++ app/src/main/java/com/caseywaldren/downfordinner/SuggestionActivity.java
@@ -1,13 +1,67 @@
 package com.caseywaldren.downfordinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseObject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SuggestionActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+
+    @Bind(R.id.etRestaurant)
+    EditText etRestaurant;
+
+    @Bind(R.id.etTime)
+    EditText etTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestion);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btnSubmit)
+    public void submitSuggestions() {
+        if("".equals(etRestaurant.getText().toString())) {
+            etRestaurant.setError("Please enter a restaurant");
+        } else if("".equals(etTime.getText().toString())) {
+            etTime.setError("Please enter a time");
+        } else {
+
+            uploadSuggestion();
+            notifyUsers();
+            startChoiceActivity();
+        }
+
+    }
+
+    public void notifyUsers() {
+        //Send push to update data set
+    }
+
+    public void uploadSuggestion() {
+        ParseObject suggestion = new ParseObject("UserData");
+        suggestion.put("restaurant", etRestaurant.getText().toString());
+        suggestion.put("restaurantVotes", 1);
+        suggestion.put("time", etTime.getText().toString());
+        suggestion.put("timeVotes", 1);
+        suggestion.saveInBackground();
+    }
+
+    public void startChoiceActivity() {
+        Intent intentStartChoiceActivity = new Intent(SuggestionActivity.this, ChoiceActivity.class);
+        startActivity(intentStartChoiceActivity);
     }
 }
