003579d10fca66efed1ef3bc2cb72a052dd1a7a0
wcohen64 <wcohen64@gmail.com>
2015-12-11 14:15:18 +0100

    working on choice activity

--- app/src/main/java/com/caseywaldren/downfordinner/ChoiceActivity.java
+++ app/src/main/java/com/caseywaldren/downfordinner/ChoiceActivity.java
@@ -2,8 +2,14 @@ package com.caseywaldren.downfordinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.caseywaldren.downfordinner.adapter.ChoiceRecyclerAdapter;
 
 public class ChoiceActivity extends AppCompatActivity {
+
+    final ChoiceRecyclerAdapter adapter = new ChoiceRecyclerAdapter(this, true, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +17,12 @@ public class ChoiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choice);
 
         setTitle("Vote for one or more restaurant:");
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager mLayoutManager =
+                new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
